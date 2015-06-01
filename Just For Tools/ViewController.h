4039f365354703e7471681fd984f21b12e2e5190
crossPQW <651024752@qq.com>
2015-06-01 22:39:06 +0800

    Initial Commit

---
+++ Just For Tools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Just For Tools
+//
+//  Created by 黄少华 on 15/6/1.
+//  Copyright (c) 2015年 黄少华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Just For Tools/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Just For Tools
+//
+//  Created by 黄少华 on 15/6/1.
+//  Copyright (c) 2015年 黄少华. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
